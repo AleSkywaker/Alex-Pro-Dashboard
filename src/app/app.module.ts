0594aec75ae6e43bba7c4f269e794eca0ca488e5
Alex Colombo <uricaine@hotmail.com>
2020-05-16 15:25:30 +0200

    setting service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { PageRoutingModule } from "./pages/pages-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
+// Custom modules
 import { AppRoutingModule } from "./app-routing.module";
 import { PagesModule } from "./pages/pages.module";
 
+// Components
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./login/register.component";
-import { FormsModule } from "@angular/forms";
+
+// Services
+import { SettingService } from "./services/setting.service";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule, PagesModule, FormsModule],
-  providers: [],
+  providers: [SettingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
